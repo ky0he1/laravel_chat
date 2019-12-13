00cbcf3256046f15b51d65b46648c0bf5d91a3d4
ky0he1 <kyouta1218@gmail.com>
2019-12-13 10:44:55 +0900

    add chat function

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,15 +5,14 @@
     <br>
     <button type="button" @click="send">送信</button>
 
-    <hr>q
+    <hr>
 
     <div v-for="m in messages">
       <span v-text="m.created_at"></span>：
       <span v-text="m.body"></span>
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+  <script src="js/app.js"></script>
   <script>
 
     new Vue({
@@ -23,14 +22,21 @@
         messages: []
       },
       mounted () {
-        axios.get('ajax/chat')
+        this.getMessage()
+        Echo.channel('chat')
+          .listen('MessageCreated', e => {
+            this.getMessage()
+          })
+      },
+      methods: {
+        getMessage () {
+          axios.get('ajax/chat')
           .then(res => {
             this.messages = res.data
             console.log(this.messages)
           })
-      },
-      methods: {
-        send: function () {
+        },
+        send () {
           const params = {message: this.message}
           axios.post('ajax/chat', params)
             .then(res => {
